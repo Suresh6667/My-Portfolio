59202b3f867d073c8c4826a134af95d6c54d5cac
Suresh6667 <suresh24612@gmail.com>
2024-12-18 20:45:55 +0530

    Initial commit: Add Next.js project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [ './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        burtons: "burtons",
+      }
+    },
+  },
+  plugins: [],
+}
